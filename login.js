198d67ab46f8049066ce50c1b60d864493f19d28
Jony-Mia <ajonymia321@gmail.com>
2026-03-07 21:32:20 +0600

    login page completed

---
+++ login.js
@@ -0,0 +1,29 @@
+
+localStorage.setItem('username',"admin");
+localStorage.setItem("password","admin123")
+
+
+const submit = document.getElementById("submit");
+const user = localStorage.getItem("username");
+const password = localStorage.getItem("password");
+
+submit.onclick = ()=>{
+    const username = document.getElementById("gitemail").value;
+    const pass = document.getElementById("password").value;
+    if(!username && !pass) return;
+
+    if(username === "admin" || pass === "admin123"){
+    window.open("http://127.0.0.1:5500","_self")
+    }
+    
+}
+
+
+if (user === "admin" && password==="admin123") {
+   window.open("http://127.0.0.1:5500","_self")
+}
+
+/**
+ * 
+ *  
+ */
