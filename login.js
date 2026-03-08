b891eee14e5ccd6f9ab7f17bb168e19a82babe0c
Jony-Mia <ajonymia321@gmail.com>
2026-03-08 13:57:29 +0600

    issue box rendering

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 
-localStorage.setItem('username',"admin");
-localStorage.setItem("password","admin123")
+// localStorage.setItem('username',"admin");
+// localStorage.setItem("password","admin123")
 
 
 const submit = document.getElementById("submit");
@@ -8,11 +8,15 @@ const user = localStorage.getItem("username");
 const password = localStorage.getItem("password");
 
 submit.onclick = ()=>{
+
+
     const username = document.getElementById("gitemail").value;
     const pass = document.getElementById("password").value;
     if(!username && !pass) return;
 
     if(username === "admin" || pass === "admin123"){
+    localStorage.setItem("username","admin")
+    localStorage.setItem("password","admin123")
     window.open("http://127.0.0.1:5500","_self")
     }
     
@@ -23,7 +27,3 @@ if (user === "admin" && password==="admin123") {
    window.open("http://127.0.0.1:5500","_self")
 }
 
-/**
- * 
- *  
- */
---
+++ main.js
@@ -0,0 +1,103 @@
+
+const issuesContainer = document.getElementById("issuesContainer")
+const issueAPI = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+// User Credential
+
+const user = localStorage.getItem("username");
+const password = localStorage.getItem("password");
+
+if (!user || !password) {
+    window.open("http://127.0.0.1:5500/login.html","_self");
+}
+
+let issues=[];
+
+async function issuesLoader(){
+   let res = await fetch(issueAPI);
+   let issuesList = await res.json();
+   issuesList.data.forEach(element => {
+    issues.push(element)
+   }); 
+    issueBox()
+}
+
+
+
+/**
+ *   {
+      "id": 1,
+      "title": "Fix navigation menu on mobile devices",
+      "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+      "status": "open",
+      "labels": [
+        "bug",
+        "help wanted"
+      ],
+      "priority": "high",
+      "author": "john_doe",
+      "assignee": "jane_smith",
+      "createdAt": "2024-01-15T10:30:00Z",
+      "updatedAt": "2024-01-15T10:30:00Z"
+    },
+ */
+function issueBox(){
+    issuesContainer.innerHTML='';
+    issues.forEach(issueData=>{
+    const date = new Date(`${issueData.createdAt}`);
+    
+          issuesContainer.innerHTML += `
+     <div class="h-full">
+                <div class="shadow p-3 py-4 rounded-tl-2xl rounded-tr-2xl border-t-4 border-${issueData.status==="open" ? "green":"violet"}-400">
+                    <div class="flex justify-between">
+                        <div>
+                            <img src="assets/${issueData.status==="open" ? "Open-Status":"Closed- Status "}.png" alt="">
+                        </div>
+                        <div>
+
+                            <p class="badge badge-error bg-error/10 text-error badge-outline">High</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 class="text-2xl my-3 font-bold" >${issueData.title}</h3>
+                        <p>${issueData.description}</p>
+                        <br>
+                        <section>
+                            <button class="btn btn-outline btn-error rounded-full"> 
+                                <i class="fa-brands fa-android"></i>
+                                 Bugs</button>
+                            <button class="btn btn-outline btn-warning rounded-full">
+                                <i class="fa-regular fa-life-ring"></i> 
+                                Help Wanted</button>
+
+                        </section>
+
+                    </div>
+                </div>
+                <div class=" p-3 shadow rounded-bl-2xl  rounded-br-2xl">
+                    <p> ${issueData.author} </p>
+                    <br>
+                    <p>${date.getDate()}/${date.getMonth()}/${date.getFullYear()}/</p>
+                </div>
+            </div>
+    
+    `;
+
+    })
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+issuesLoader()
